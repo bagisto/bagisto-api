2b2735cff518da0c994ed4c01728c864ff66aede
Navneet Kumar - Webkul <navneetkumar.symfony813@webkul.in>
2026-01-23 14:02:54 +0530

    Update InstallApiPlatformCommand.php

--- src/Console/Commands/InstallApiPlatformCommand.php
+++ src/Console/Commands/InstallApiPlatformCommand.php
@@ -39,6 +39,8 @@ class InstallApiPlatformCommand extends Command
 
             $this->runDatabaseMigrations();           
 
+            $this->clearAndOptimizeCaches();
+            
             $this->generateApiKey();
 
             $this->publishConfiguration();

9f0537de04d946b754c085e340afe2937115e00d
Yashvir Singh <yashvir.singh322@webkul.in>
2026-07-14 18:26:33 +0530

    Update playwright config file

--- tests/e2e-pw/playwright.config.ts
+++ tests/e2e-pw/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     ['html'],
   ],
   webServer: {
-    command: `cd ${BAGISTO_PATH} && APP_ENV=testing php artisan serve --host=127.0.0.1 --port=8000`,
+    command: `cd ${BAGISTO_PATH} && php artisan serve --host=127.0.0.1 --port=8000`,
     url: 'http://127.0.0.1:8000',
     reuseExistingServer: !process.env.CI,
     timeout: 60_000,
